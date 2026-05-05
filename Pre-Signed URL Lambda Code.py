466cacfbe9f67bb8745420e86abb5c04d7b472bc
nparanth <naveenparanthaman1@gmail.com>
2026-05-04 20:01:33 -0400

    Update Pre-Signed URL Lambda Code.py

--- Pre-Signed URL Lambda Code.py
+++ Pre-Signed URL Lambda Code.py
@@ -1,17 +1,18 @@
 import json
 import boto3
+import os
 
 
 s3 = boto3.client('s3')
-bucket_name = "example"
+bucket_name = os.environ.get('UPLOAD_BUCKET', 'example')
 
 def lambda_handler(event, context):
  
     
     # Get filename from the frontend request
-    body = json.loads(event.get('body', '{}')) # ramndom value after comma is used in case there is not an actual 'body' value
-    file_name = body.get('fileName', 'unknown_asset.stl')
-    content_type = body.get('contentType', 'application')
+    body = json.loads(event.get('body', '{}')) # fallback value after comma is used in case there is not an actual 'body' value
+    file_name = body.get('fileName', '{})
+    content_type = body.get('contentType', '{}')
 
     # Generate the pre-signed URL (Valid for 5 minutes)
     url = s3.generate_presigned_url('put_object',
